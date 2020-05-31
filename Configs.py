1f292231577e438d39e9cbb4882a4f45fe69c622
eugene22z <46564310+eugene22z@users.noreply.github.com>
2020-05-31 19:18:52 +0300

    Add files via upload

---
+++ Configs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+first_token = '1251587981:AAHtr1P_OitR86lnJUWgtbqy7W7vikqo_1M'
+second_token = '1299124903:AAEyyV8lDo0Y9aMWRUBH6corOYYWeXYbJ_Q'
+
+
+# In[ ]:
+
+
+
+
